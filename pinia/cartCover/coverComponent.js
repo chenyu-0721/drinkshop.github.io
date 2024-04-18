dc295e8e490797e02b5d3d3b7cf83c0ca8d8e93d
陳冠宇 <chen630602@gmail.com>
2024-04-18 22:39:42 +0800

    更改

--- pinia/cartCover/coverComponent.js
+++ pinia/cartCover/coverComponent.js
@@ -4,41 +4,56 @@ const { mapState } = Pinia;
 
 export default {
   template: ` 
-
-  <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark" >
-    <div class="container-fluid" >
-      <div class="collapse navbar-collapse justify-content-center" id="navbarCollapse">
-        <ul class="navbar-nav ">
-          <li class="nav-item">
-            <a class="navbar-brand" href="#">商品頁面</a>
-          </li>
-          <li class="nav-item">
-            <a class="navbar-brand" href="#/blacktea">紅茶</a>
-           </li> 
-          <li class="nav-item">
-            <a class="navbar-brand" href="#/milkTea">奶茶</a>
-          </li> 
-          <li class="nav-item">
-            <a class="navbar-brand" href="#/tea">綠茶</a>
-          </li> 
-          <li class="nav-item">
-          <a class="navbar-brand" href="#/ice">冰沙</a>
-        </li>
-
-          <li class="nav-item">
-            <a class="navbar-brand" href="#/other">其他飲品</a>
-          </li> 
-        </ul>    
-      </div> 
-      <router-link to="/card" class="btn btn-dark" style="font-size:20px">
-      購物車
-      <span class="badge rounded-pill bg-danger text-white">{{ card.length }}</span>
-      </router-link>
+  <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+    <div class="container " >
+        <a class="navbar-brand" href="#">商品頁面</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+            data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false"
+            aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse " id="navbarCollapse">
+            <ul class="navbar-nav me-auto mb-2 mb-md-0">
+              <li class="nav-item ">
+                <a class="navbar-brand" href="#/blacktea">紅茶</a>
+              </li>
+              <li class="nav-item">
+                <a class="navbar-brand" href="#/milkTea">奶茶</a>
+              </li>
+              <li class="nav-item">
+                <a class="navbar-brand" href="#/tea">綠茶</a>
+              </li>
+              <li class="nav-item">
+                <a class="navbar-brand" href="#/ice">冰沙</a>
+              </li>
+              <li class="nav-item">
+                <a class="navbar-brand" href="#/other">其他飲品</a>
+              </li>
+            </ul>
+            <form class="d-flex">
+              <router-link to="/card" class="btn btn-dark" style="font-size:20px">
+                購物車
+                <span class="badge rounded-pill bg-danger text-white">{{ card.length }}</span>
+              </router-link>
+            </form>
+        </div>
     </div>
   </nav>
+
+
   
     `,
   computed: {
     ...mapState(cardStore, ["card"]),
   },
 };
+
+//   <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark" >
+
+//     <div class="collapse navbar-collapse justify-content-center" id="navbarCollapse">
+// <ul class="navbar-nav ">
+
+//       </ul>
+
+//   </div>
+// </nav>
--- pinia/homeworkComponent/productsComponent.js
+++ pinia/homeworkComponent/productsComponent.js
@@ -10,11 +10,11 @@ export default {
 
   template: `  
   <div id="carouselExampleIndicators" class="carousel slide carousel-fade" data-bs-ride="carousel">
-  <div class="carousel-indicators">
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
-    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-  </div>
+    <div class="carousel-indicators">
+      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    </div>
   <div class="carousel-inner">
     <div class="carousel-item active">
     <img style="width:100%; height:600px; object-fit: cover; " src="https://images.unsplash.com/photo-1514362545857-3bc16c4c7d1b?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" class="d-block w-100" alt="...">
@@ -37,7 +37,7 @@ export default {
 </div>
   
   
-<div class="album py-5 bg-light">
+<div class="album bg-light">
   <div class="container">
       <div class="row row-cols-1 row-cols-sm-2 row-cols-md-4 g-3 my-4">
           <div class="col" v-for="product in sortProducts" :key="product.id">
